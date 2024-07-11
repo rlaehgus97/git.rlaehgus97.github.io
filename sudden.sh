9b09f72687a31a240cc07ffd72dcfb3589631fba
rlaehgus97 <starkim625@gmail.com>
2024-07-11 12:15:12 +0900

    시간 동기화

--- sudden.sh
+++ sudden.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+figlet KIA
+
 FILE=~/tmp/_SUDDEN
+#MODEL=KIA
+#DATE=$(date +"%Y%m%d %H:%M:%S")
 
 while true
 do
-     if [[ -f "$FILE" ]]; then
-	     echo "SUDDEN!!"
-     else
-	 date
-     fi
-     sleep 3
+        if [[ -f "$FILE" ]]; then
+        #       echo "check $FILE"
+                echo "sudden!!!"
+        #       sleep 3
+        else
+                figlet KIA
+                DATE=$(date +"%Y%m%d %H:%M:%S")
+        #       echo "error"
+                echo $DATE, SUDDEN!!
+        fi
+        sleep 3
 done
